1a40dda4abf97a93789cb5d7da997f5a72310c3d
kenyetta <kenyetta@student.21-school.ru>
2024-08-05 17:10:24 +0300

    Переделан для поиска по всем полям одновременно

--- src/main/java/shop/household/client/get/service/ClientService.java
+++ src/main/java/shop/household/client/get/service/ClientService.java
@@ -1,5 +1,11 @@
 package shop.household.client.get.service;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import shop.household.client.get.entity.Client;
@@ -17,30 +23,31 @@ import java.util.Objects;
 @Service
 @RequiredArgsConstructor
 public class ClientService {
-
+    @PersistenceContext
+    private EntityManager entityManager;
     private final ClientRepository clientRepository;
 
     public ClientGetResponseDto getClients(ClientGetRequestDto clientDto) {
         List<Client> clients = new ArrayList<>();
         if (Objects.nonNull(clientDto.getId())) {
             return getById(clientDto.getId());
-        } else
-        if (Objects.nonNull(clientDto.getFirstname())) {
-            clients.addAll(clientRepository.findByFirstnameContainingIgnoreCase(clientDto.getFirstname()));
-        } else if (Objects.nonNull(clientDto.getLastname())) {
-            clients.addAll(clientRepository.findByLastnameContainingIgnoreCase(clientDto.getLastname()));
         } else if (Objects.nonNull(clientDto.getEmail())) {
             clientRepository.findByEmailIgnoreCase(clientDto.getEmail()).ifPresent(clients::add);
-        } else if (Objects.nonNull(clientDto.getCreateAt())) {
-            if (Objects.isNull(clientDto.getCreateAt().getBegin())) {
-                clientDto.getCreateAt().setBegin(new Timestamp(0));
-            }
-            if (Objects.isNull(clientDto.getCreateAt().getEnd())) {
-                clientDto.getCreateAt().setEnd(new Timestamp(System.currentTimeMillis()));
-            }
-            clients.addAll(clientRepository.findByCreateAtBetween(clientDto.getCreateAt().getBegin(), clientDto.getCreateAt().getEnd()));
+//        } else if (Objects.nonNull(clientDto.getFirstname())) {
+//            clients.addAll(clientRepository.findByFirstnameContainingIgnoreCase(clientDto.getFirstname()));
+//        } else if (Objects.nonNull(clientDto.getLastname())) {
+//            clients.addAll(clientRepository.findByLastnameContainingIgnoreCase(clientDto.getLastname()));
+//        } else if (Objects.nonNull(clientDto.getCreateAt())) {
+//            if (Objects.isNull(clientDto.getCreateAt().getBegin())) {
+//                clientDto.getCreateAt().setBegin(new Timestamp(0));
+//            }
+//            if (Objects.isNull(clientDto.getCreateAt().getEnd())) {
+//                clientDto.getCreateAt().setEnd(new Timestamp(System.currentTimeMillis()));
+//            }
+//            clients.addAll(clientRepository.findByCreateAtBetween(clientDto.getCreateAt().getBegin(), clientDto.getCreateAt().getEnd()));
         } else {
-            clients.addAll(clientRepository.findAll());
+            clients.addAll(getClientsByFilter(clientDto));
+//            clients.addAll(clientRepository.findAll());
         }
         if (clients.isEmpty()) {
             return new ClientGetResponseDto()
@@ -51,6 +58,27 @@ public class ClientService {
         return new ClientGetResponseDto()
                 .status(true)
                 .clients(clients.stream().map(ClientMapper.INSTANCE::clientToClientDto).toList());
+    }
+
+    public List<Client> getClientsByFilter(ClientGetRequestDto clientDto) {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Client> criteriaQuery = criteriaBuilder.createQuery(Client.class);
+        Root<Client> root = criteriaQuery.from(Client.class);
+        List<Predicate> predicates = new ArrayList<>();
+        if (Objects.nonNull(clientDto.getFirstname())) {
+            predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("firstname")), "%" + clientDto.getFirstname().toLowerCase() + "%"));
+        }
+        if (Objects.nonNull(clientDto.getLastname())) {
+            predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("lastname")), "%" + clientDto.getLastname().toLowerCase() + "%"));
+        }
+        criteriaQuery.select(root).where(
+                predicates.toArray(new Predicate[0])
+//                    cb.and(
+//                            cb.equal(root.get("firstname"), clientGetRequestDto.getFirstname()),
+//                            cb.equal(root.get("lastname"), clientGetRequestDto.getLastname())
+//                    )
+            );
+        return entityManager.createQuery(criteriaQuery).getResultList();
     }
 
     public ClientGetResponseDto getById(Integer id) {
